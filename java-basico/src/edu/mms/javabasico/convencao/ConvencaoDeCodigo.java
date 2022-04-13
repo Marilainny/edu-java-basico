bf9bd0405e4558b050f659afd715e9ed07de5869
Marilainny <jkjrodrigues@hotmail.com>
2022-04-13 00:20:45 -0300

    Commit inicial edu-java-basico

---
+++ java-basico/src/edu/mms/javabasico/convencao/ConvencaoDeCodigo.java
@@ -0,0 +1,18 @@
+package edu.mms.javabasico.convencao;
+
+/**
+* A classe ConvencaoDeCodigo apresenta na prática convenções do Java para programação, 
+* refatoração de código, e padrões utilizados no Java.
+* 
+* {@link https://docs.oracle.com/en/java/javase/18/javadoc/javadoc.html}
+* 
+* @author  Marilainny Martins da Silva
+* @version 1.0
+* @since   2020.04.11 
+*/
+
+public class ConvencaoDeCodigo {
+	
+	
+
+}
